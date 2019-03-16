6ec8dfd194fa783263156d31088e4f7e128c0d86
Dmitry Savchenkov <ds@notagency.ru>
2019-03-16 05:04:05 +0300

    update webpack config and build

--- webpack.config.js
+++ webpack.config.js
@@ -2,31 +2,35 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
-module.exports = {
-  entry: {
-    app: './src/index.js'
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    hot: true
-  },
-  module: {
-    rules: [
-      {
-        test: /\.html$/,
-        use: 'html-loader'
-      },
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Hot Module Replacement',
-      template: 'index.html'
-    }),
-    new webpack.HotModuleReplacementPlugin()
-  ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'assets')
+module.exports = (env, options) => {
+  let config = {
+    entry: {
+      app: './src/index.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.html$/,
+          use: 'html-loader'
+        },
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'index.html'
+      }),
+    ],
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'assets')
+    }
+  };
+  if (options.mode !== 'production') {
+    config.devtool = 'inline-source-map';
+    config.devServer = {
+      hot: true
+    };
+    config.plugins.push(new webpack.HotModuleReplacementPlugin())
   }
+  return config
 };
