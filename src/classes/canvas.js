5ac030e428a69827438dce7bfa003292b32c6cb5
Dmitry Savchenkov <ds@notagency.ru>
2019-03-17 23:52:21 +0300

    apply canvas position settings

--- src/classes/canvas.js
+++ src/classes/canvas.js
@@ -4,14 +4,19 @@ import Config from '../json/config';
 const scaleFactor = 1;
 
 export const canvas = document.getElementById('canvas');
-const {width, height} = Config.layout.canvas;
+const {width, height, offsetTop, offsetLeft} = Config.layout.canvas;
 canvas.width = width * scaleFactor;
 canvas.height = height * scaleFactor;
-// toDo offset top & offset left
 canvas.style.width = width + 'px';
 canvas.style.height = height + 'px';
+canvas.style.top = offsetTop + 'px';
+canvas.style.left = offsetLeft + 'px';
 
 export const ctx = canvas.getContext('2d');
 ctx.scale(scaleFactor, scaleFactor);
 
-export const boundingClientRect = canvas.getBoundingClientRect();
+let boundingClientRect = canvas.getBoundingClientRect();
+
+window.addEventListener('resize', () => boundingClientRect = canvas.getBoundingClientRect());
+
+export { boundingClientRect };
